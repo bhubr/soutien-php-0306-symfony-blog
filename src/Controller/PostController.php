fb7115aa62329bb42b0d928c2e36c19d30cef9a4
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-06-03 20:02:00 +0200

    Add route that display a post's details

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
@@ -11,7 +12,7 @@ class PostController extends AbstractController
     /**
      * @Route("/", name="post_list")
      */
-    public function index()
+    public function index(): Response
     {
         $posts = $this->getDoctrine()
             ->getRepository(Post::class)
@@ -20,4 +21,18 @@ class PostController extends AbstractController
             'posts' => $posts,
         ]);
     }
+
+    /**
+     * @Route("/{slug<^[a-z0-9-]+$>}",name="post_details")
+     */
+    public function details(string $slug): Response
+    {
+        $post = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->findOneBy(['slug' => $slug]);
+        return $this->render('post/details.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
 }
