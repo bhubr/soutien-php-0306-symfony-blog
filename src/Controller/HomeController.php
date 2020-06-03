88cd0e6a44aaefbd367b39dd06692dded1a7df8d
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-06-03 19:16:24 +0200

    Get all posts and display them on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,11 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $posts = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'posts' => $posts,
         ]);
     }
 }
